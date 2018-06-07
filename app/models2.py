a986024f2d63172802017a96cd0286bfad019238
patrick migot <ptrckmigot@gmail.com>
2018-06-07 20:42:07 +0300

    [Feature #158124218] Add a login route

--- app/models2.py
+++ app/models2.py
@@ -1,31 +1,21 @@
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 GET_USERS = "SELECT * FROM users"
 UPDATE_REQUESTS = ""
 FETCH_ALL_REQUESTS = "SELECT * FROM requests"
 GET_A_SINGLE_REQUEST = "SELECT * FROM requests where id='id'"
 GET_USERNAME= "SELECT username FROM users"
+GET_TOKEN = "SELECT token FROM users"
 
 
 conn = psycopg2.connect("dbname='maintraq' user='postgres' host='localhost' password='myadmin01?'")
 
-cur = conn.cursor()
-
-def get_users(username):
-    #cur.execute(GET_USERS)
-   cur.execute(GET_USERS)
-   items = cur.fetchall()
-   conn.commit()
-   return items
+cur = conn.cursor(cursor_factory=RealDictCursor)
 
 
 def create_user(user):
-   cur.execute("INSERT INTO USERS (first_name,last_name,email,username,password) values(%s,%s,%s,%s,%s)",(
-       user['first_name'] ,
-       user['last_name'],
-       user['email'],
-       user['username'],
-       user['password']))
+   cur.execute("INSERT INTO USERS (first_name,last_name,email,username,password) values(%(first_name)s, %(last_name)s,%(email)s,%(username)s,%(password)s)",(user))
    conn.commit()
 
 def get_username(username):
@@ -38,15 +28,25 @@ def get_user(username):
     cur.execute("SELECT * FROM USERS WHERE username = %s", (username,))
     item = cur.fetchone()
     conn.commit()
-    print(item)
-    return {
-        "id": item[0],
-        "first_name": item[1],
-        "last_name": item[2],
-        "email": item[3],
-        "username": item[4],
-        "password": item[5]
-    }
+    return item
+
+
+def create_request(requests):
+    cur.execute("INSERT INTO REQUESTS (title,location,body,UserID) values(%s,%s,%s,%s)",(
+       requests['title'] ,
+       requests['location'],
+       requests['body'],
+       requests['UserID']))
+
+def get_title(title):
+    cur.execute("SELECT * FROM REQUESTS WHERE title = %s", (title,))
+    item = cur.fetchone()
+    conn.commit()
+    return item
+        
+    
+
+
 
    
    
--- app/views2.py
+++ app/views2.py
@@ -10,8 +10,28 @@ import re
 
 import uuid
 
-from app.models2 import create_user ,get_username, get_user
+from app.models2 import create_user , get_user,get_title,get_username,create_request
+def login_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'access-token' not in request.headers:
+            return jsonify({
+                'message': 'Token is missing, login to get token'
+            }), 401
+        try:
+            token = request.headers['access-token']
+            data = jwt.decode(token, app.config["SECRET_KEY"])
+            username = data['username'].strip()
+            current_user = get_user(username)
+            if not current_user:
+                return jsonify({"message": "You are not logged in"}), 401
+            
+        except:
+            return jsonify({'message': 'Token is invalid or Expired!'}), 401
 
+        return f(current_user, *args, **kwargs)
+    return decorated
 
 @app.route('/api/v2/auth/register', methods=['POST'])
 def register():
@@ -44,6 +64,7 @@ def login():
     auth = request.get_json()
     if not auth or not auth['username'].strip() or not auth['password']:
         return jsonify({"message": "login required!"}), 401
+
     user = get_user(auth['username'])
     if user is None:
         return jsonify({"message": "User does not exist"}), 404
@@ -54,6 +75,34 @@ def login():
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=1000)},
             app.config["SECRET_KEY"]
         )
-        # user_info.user_token[user['username']] = token.decode('UTF-8')
+        #get_token[user['username']] = token.decode('UTF-8')
         return jsonify({"auth_token": token.decode('UTF-8')}), 200
     return jsonify({"message": "Wrong password!"}), 401
+
+@app.route('/api/v2/Auth/request', methods=['POST'])
+@login_required
+def create_requests(current_user):
+    """endpoint to create a new request"""
+    
+    data = request.get_json()
+    #if not data or not data['title'].strip():
+        #return jsonify({"message": "Name cannot be empty!"}), 401
+    #title=data['title'].strip()
+    #for req in get_title(title):
+        #if data['title'].strip() == req['title']:
+           # return jsonify({"message": "Sorry!! Name taken!"}), 401
+    # create request
+    UserID = current_user['id']
+    
+    requests = {
+    "title": data['title'].strip(),
+    "location": data['location'],
+    "body": data['body'],
+    "UserID" : UserID
+    
+    }
+    create_request(requests)
+    return jsonify({
+        "message": "Request created", 'request': requests
+    }), 201
+   
