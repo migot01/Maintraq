51c18f149879432718440a9da4aff9ba66aee50a
patrick migot <patrickmigot@patricks-MacBook-Pro.local>
2018-07-05 19:57:05 +0300

    [chore] Refactor code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 from app.views2 import views2
 app.register_blueprint(views2)
 from config import app_config
-app.config['SECRET_KEY'] = 'thisissecret'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY_API')
